daf76bfe55bb43e6415dc9364cfa78771bffa1b7
Gregor Feigel <gregor.feigel@icloud.com>
2023-12-12 21:28:15 +0100

    suport  for better file handling

---
+++ Tests/VirtualDataSourceTests/Test_DB.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Gregor Feigel on 12.12.23.
+//
+
+import Foundation
+
+import XCTest
+@testable import VirtualDataSource
+
+final class VirtualDataSource_Test: XCTestCase {
+    
+    func test_db() throws {
+        
+        print("test 123")
+    }
+    
+}
